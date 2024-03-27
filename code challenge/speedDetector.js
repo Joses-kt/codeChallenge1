88eec80a08224318fef791948497793a01d276b8
Joses-kt <162096405+Joses-kt@users.noreply.github.com>
2024-03-27 16:07:55 +0300

    Update speedDetector.js

--- code challenge/speedDetector.js
+++ code challenge/speedDetector.js
@@ -1,26 +1,19 @@
-//SPEED DETECTOR
-function calculateDemeritPoints(speed) {
-    const speedLimit = 70;
-    let demeritPoints = 0;
-
-    if (speed > speedLimit) {
-      demeritPoints = Math.floor((speed - speedLimit) / 5);
+//Spped Detector
+// Prompt the user to input the speed in km/h
+const prompt = require("prompt-sync")({ sigint: true });
+const speed = parseFloat(prompt("Enter car speed (in km/h):"));
+// Give the points a starting point (haha ikr)
+let points = 0;
+// Check speed limit
+ if (speed <=70){
+    console.log('Ok')
+ } 
+// Calculate and accumulate points: Define warning if points exceeded
+ else if (speed >70){
+    points = Math.abs((speed -70)/5)
+    console.log('Points:' + points)
+    if (points >=12){ 
+        console.log("License suspended")
     }
+ }
 
-    return demeritPoints;
-  }
-
-  function main() {
-    const speed = parseInt(prompt("Enter the speed of the car (in km/h):"));
-
-    const demeritPoints = calculateDemeritPoints(speed);
-
-    if (demeritPoints > 0) {
-      console.log("Points:", demeritPoints);
-      if (demeritPoints > 12) {
-        console.log("License suspended");
-      }
-    } else {
-      console.log("Ok");
-    }
-  }
